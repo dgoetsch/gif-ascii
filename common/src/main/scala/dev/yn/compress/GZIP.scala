3bb0c009aee8d6060e3e36c1722b3ab8cb5b8d7c
Devyn Goetsch <devynmichellegoetsch@gmail.com>
2016-08-12 21:26:51 -0500

    rancher poc, may work

--- common/src/main/scala/dev/yn/compress/GZIP.scala
+++ common/src/main/scala/dev/yn/compress/GZIP.scala
@@ -4,21 +4,22 @@ import java.io._
 import java.util.zip.{GZIPInputStream, GZIPOutputStream}
 
 object GZIP {
-  def outputStream(s: String): ByteArrayOutputStream = {
-    val outputStream = new ByteArrayOutputStream(s.length)
+  def compressedOutputStream(s: String): NonCopyingByteArrayOutputStream = {
+    val outputStream = new NonCopyingByteArrayOutputStream(s.length)
     val writer = new BufferedWriter(new OutputStreamWriter(new GZIPOutputStream(outputStream)));
     writer.append(s)
     writer.close()
     outputStream
   }
 
-  def write(s: String): Array[Byte] = {
-    outputStream(s).toByteArray
+  def compress(s: String): Array[Byte] = {
+    val outputStream = compressedOutputStream(s)
+    outputStream.getBuf.take(outputStream.getCount)
   }
 
-  def byteStream(gzip: Array[Byte]) = {
+  def decompressedOutputStream(gzip: Array[Byte]) = {
     val inputStream = new GZIPInputStream(new ByteArrayInputStream(gzip))
-    val out = new ByteArrayOutputStream()
+    val out = new NonCopyingByteArrayOutputStream(gzip.length * 10)
     val buffer: Array[Byte] = new Array(1024)
     var in = inputStream.read(buffer)
     while(in > 0) {
@@ -28,7 +29,16 @@ object GZIP {
     out
   }
 
-  def read(bytes: Array[Byte]): String = {
-    new String(byteStream(bytes).toByteArray)
+  def decompress(bytes: Array[Byte]): String = {
+    val outputStream = decompressedOutputStream(bytes)
+    new String(outputStream.getBuf.take(outputStream.getCount))
   }
+}
+
+/**
+  * Byte array outpust stream that allows direct access to the buffer
+  */
+class NonCopyingByteArrayOutputStream(size: Int) extends ByteArrayOutputStream(size) {
+  def getCount = count
+  def getBuf = buf
 }
